b17306dd279994dd9412148443a8a1c81a6f0092
Jose Stephen Peña <jose.pena@cybersoftbpo.com>
2022-03-27 17:09:42 +0800

    add validation method to appointments

--- app/models/appointment.rb
+++ app/models/appointment.rb
@@ -12,5 +12,15 @@
 #
 class Appointment < ApplicationRecord
   belongs_to :user
+  belongs_to :patient, class_name: 'User', foreign_key: 'user_id'
   belongs_to :clinic
+
+  validates :schedule, uniqueness: true
+  validate :only_one_appointment_in_a_day, on: :create
+
+  def only_one_appointment_in_a_day
+    if user.appointments_in_a_day(schedule) >= 1
+      errors.add(:base, "Patient cannot have more than 1 appointment a day.")
+    end
+  end
 end
